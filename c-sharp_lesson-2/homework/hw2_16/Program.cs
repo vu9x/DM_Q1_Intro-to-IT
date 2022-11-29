e58933091f94a2c04bcc8d5b098932ac1b2a0542
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-29 08:39:20 +0300

    hw2_16 done

---
+++ c-sharp_lesson-2/homework/hw2_16/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+int DayOfWeek = new Random().Next(1,7);
+
+bool Weekend(int a){
+    if(a >=6) return true;
+    else return false;
+}
+if(Weekend(DayOfWeek)) Console.WriteLine($"Day {DayOfWeek} is a weekend");
+else Console.WriteLine($"Day {DayOfWeek} is not a weekend");
