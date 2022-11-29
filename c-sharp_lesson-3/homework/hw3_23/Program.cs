5f1d699ae78fc291a907f8706bf3a47a1b8f2a23
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-29 19:20:05 +0300

    hw3_23 done

---
+++ c-sharp_lesson-3/homework/hw3_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void FindCubePower(int number){
+    for(int i = 1; i <= number; i++){
+        double SquareNumber = Math.Pow(i, 3);
+        Console.Write(SquareNumber + ", ");
+    }
+}
+
+Console.Write("Введите число от 1 до 50: ");
+int x = Convert.ToInt32(Console.ReadLine());
+FindCubePower(x);
