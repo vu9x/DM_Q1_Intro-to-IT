06690bc6a333bc836844a8c2a5f217ab45998bb0
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-29 13:59:00 +0300

    hw3_19 done

---
+++ c-sharp_lesson-3/homework/hw3_19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+bool Palindrom(int number){
+    int first_num = number / 10000;
+    int second_num = number / 1000 % 10;
+    int fourth_num = number % 100 / 10;
+    int fifth_num = number % 10;
+    if(first_num == fifth_num & second_num == fourth_num) return true;
+    else return false;
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 9999 & num < 100000){
+    if(Palindrom(num)) Console.WriteLine($"Введеное число {num} является палиндромом");
+    else Console.WriteLine($"Введеное число {num} не является палиндромом");
+}
+else Console.WriteLine($"Введеное число {num} не является пятизначным");
