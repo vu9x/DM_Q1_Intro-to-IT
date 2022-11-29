c42f3f6de3d0a9a4cd3fde523691bef8b46eff24
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-29 06:35:50 +0300

    hw2_10 done

---
+++ c-sharp_lesson-2/homework/hw2_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+int FindSecondDigit(int number)
+{
+    int result = number % 100 / 10;
+    return result;
+}
+
+int RandNumb = new Random().Next(100,1000);
+int SecondDigit = FindSecondDigit(RandNumb);
+
+Console.WriteLine($"Second digit of number {RandNumb} is {SecondDigit}");
