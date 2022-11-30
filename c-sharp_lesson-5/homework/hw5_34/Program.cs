dc5f40bd88e8af964a3e70a646d36655c51d379d
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-30 11:55:56 +0300

    hw5_34 done

---
+++ c-sharp_lesson-5/homework/hw5_34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] CreateNewArray(int size){
+    int [] arr = new int[size];
+    for(int i = 0; i < size; i++){
+        arr[i] = new Random().Next(1000, 10000);
+    }
+    return arr;
+}
+
+void Show(int [] arr){
+
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++){
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine("\b]");
+}
+
+int EvenElements(int [] arr){
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i] % 2 == 0){
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int [] MyArray = CreateNewArray(4);
+Show(MyArray);
+int EvenNumbers = EvenElements(MyArray);
+Console.WriteLine($"Even numbers is {EvenNumbers}");
+
