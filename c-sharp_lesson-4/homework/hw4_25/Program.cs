6536c4b439d190d93be9d70e95d1684dfb759a99
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-30 05:17:06 +0300

    hw4_25 done

---
+++ c-sharp_lesson-4/homework/hw4_25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A 
+//в натуральную степень B (Math.Pow НЕ использовать)
+
+int Pow(int number, int power){
+    int result = 1;
+    for(int i = 1; i <= power; i++){
+        result *= number;
+    }
+    return result;
+}
+
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input power: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int NewNumber = Pow(num, power);
+
+Console.WriteLine($"{num}^{power} = {NewNumber}");
