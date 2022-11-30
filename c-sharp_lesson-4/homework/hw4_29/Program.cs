2faac78dcc5f8c50a5bb27c0025cadc43fcf85ac
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-11-30 06:19:23 +0300

    hw4_29 done

---
+++ c-sharp_lesson-4/homework/hw4_29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая создает массив заданного пользователем размера, 
+// заполняет массив элементами от 1 до 99 и выводит их на экран.
+
+int [] CreateRandArray(int n){
+    int [] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int [] arr){
+
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++){
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("\b\b]");
+}
+
+Console.WriteLine("Input a lenght of your Array: ");
+int num = Convert.ToInt32(Console.ReadLine());
+PrintArray(CreateRandArray(num));
