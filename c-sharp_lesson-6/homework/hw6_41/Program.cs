c67d466d1f5c7a12744066da431bfdb7fc08fea5
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-12-01 06:49:30 +0300

    hw6_41 done

---
+++ c-sharp_lesson-6/homework/hw6_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveCounter(int N){
+    int counter = 0;
+    for(int i = 0; i < N; i++){
+        Console.WriteLine($"Введите ваше {i+1} число массива: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if(number > 0) counter++;
+    }
+    return counter;
+}
+
+Console.WriteLine($"Введите размер массива массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int numbers = PositiveCounter(size);
+Console.WriteLine($"Вы ввели {numbers} положительных чисел");
