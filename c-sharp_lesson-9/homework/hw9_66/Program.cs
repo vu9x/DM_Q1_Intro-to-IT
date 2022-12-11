70c954db4586cb3220ec4fa23f3d1f693326596e
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-12-11 20:35:03 +0300

    hw9_66 done

---
+++ c-sharp_lesson-9/homework/hw9_66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+int ArithmeticProgression(int M, int N){
+    if(M != N) return N + ArithmeticProgression(M, N-1);
+    else return N;
+}
+
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int number = ArithmeticProgression(m, n);
+Console.WriteLine($"Arithmetic Progression between {m} and {n} = {number}");
