6d8ca2b5f6ac3c64c429f2eed277931785b6a94d
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2022-12-11 19:32:17 +0300

    hw9_64 done

---
+++ c-sharp_lesson-9/homework/hw9_64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void NaturalFigures(int N){
+    if(N > 0){
+        Console.Write($"{N} ");
+        NaturalFigures(N-1);
+    }
+}
+
+Console.Write("Input natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalFigures(number);
